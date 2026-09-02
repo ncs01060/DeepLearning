e9ea544ff15d80451fea69adc2e2e3e3c7886153
ncs01060 <ncs01045@gmail.com>
2026-09-02 16:02:48 +0900

    순전파, 역전파 구현

---
+++ ch05/buy_apple.py
@@ -0,0 +1,22 @@
+from layer_native import MulLayer
+apple = 100
+apple_num = 2
+tax = 1.1
+
+# 계층들
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forword(apple,apple_num)
+price = mul_tax_layer.forword(apple_price, tax)
+
+print(price) # 220
+
+# 역전파
+
+dprice = 1
+dapple_price,dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple, dapple_num, dtax)
